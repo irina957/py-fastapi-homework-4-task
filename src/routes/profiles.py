24083c12754e2e53d8f9cc6c991a7e4167052b4d
iryna <irakirvas78@gmail.com>
2026-04-15 11:49:08 +0300

    Implement py-fastapi-homework-4-task

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -3,17 +3,17 @@ from fastapi import (
     Depends,
     HTTPException,
     status,
-    Request,
 )
 from typing import Annotated
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
-from schemas.profiles import ProfileCreateRequestSchema, ProfileCreateResponseSchema
+from schemas.profiles import ProfileCreateSchema, ProfileResponseSchema
 from database import get_db, UserProfileModel, UserModel, UserGroupEnum
 from config.dependencies import get_s3_storage_client, get_jwt_auth_manager
 from security.interfaces import JWTAuthManagerInterface
+from security.http import get_token
 from exceptions import TokenExpiredError, InvalidTokenError, S3FileUploadError
 from storages import S3StorageInterface
 
@@ -21,31 +21,16 @@ router = APIRouter()
 
 
 async def get_current_user(
-    request: Request,
+    token: str = Depends(get_token),
     jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
     db: AsyncSession = Depends(get_db),
 ) -> UserModel:
-    auth_header = request.headers.get("Authorization")
-    if not auth_header:
-        raise HTTPException(status_code=401, detail="Authorization header is missing")
-    if not auth_header.startswith("Bearer "):
-        raise HTTPException(
-            status_code=401,
-            detail="Invalid Authorization header format. Expected 'Bearer <token>'",
-        )
-
-    token = auth_header.split("Bearer ")[1]
-
     try:
         payload = jwt_manager.decode_access_token(token)
     except TokenExpiredError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired."
-        )
+        raise HTTPException(status_code=401, detail="Token has expired.")
     except InvalidTokenError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token."
-        )
+        raise HTTPException(status_code=401, detail="Invalid token.")
 
     user_id = payload.get("user_id")
     if not user_id:
@@ -67,12 +52,14 @@ async def get_current_user(
     return user
 
 
-@router.post("/users/{user_id}/profile/", status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/users/{user_id}/profile/",
+    response_model=ProfileResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
 async def create_profile(
     user_id: int,
-    profile_data: Annotated[
-        ProfileCreateRequestSchema, Depends(ProfileCreateRequestSchema.as_form)
-    ],
+    profile_data: Annotated[ProfileCreateSchema, Depends(ProfileCreateSchema.as_form)],
     current_user: Annotated[UserModel, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(get_db)],
     s3_client: Annotated[S3StorageInterface, Depends(get_s3_storage_client)],
@@ -120,9 +107,10 @@ async def create_profile(
 
     db.add(profile)
     await db.commit()
+    await db.refresh(profile)
     avatar_url = await s3_client.get_file_url(profile.avatar)
 
-    return ProfileCreateResponseSchema(
+    return ProfileResponseSchema(
         id=profile.id,
         user_id=profile.user_id,
         first_name=profile.first_name,
